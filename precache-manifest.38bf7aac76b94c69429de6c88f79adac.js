e516ed2ed8b619151ddcf030902c51aaaaac1313
Amaan Saeed <amaansaeed@Guava.local>
2019-01-02 19:59:07 -0500

    Updates

---
+++ precache-manifest.38bf7aac76b94c69429de6c88f79adac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f197217cd34f61d07aa0",
+    "url": "/the-known-world/static/css/main.f0b5c864.chunk.css"
+  },
+  {
+    "revision": "f197217cd34f61d07aa0",
+    "url": "/the-known-world/static/js/main.f197217c.chunk.js"
+  },
+  {
+    "revision": "d10668c995d2f1c1869a",
+    "url": "/the-known-world/static/js/1.d10668c9.chunk.js"
+  },
+  {
+    "revision": "a1565f88dc49a2c90d9c",
+    "url": "/the-known-world/static/js/runtime~main.a1565f88.js"
+  },
+  {
+    "revision": "47494475c851888fc272be7eb9aef82c",
+    "url": "/the-known-world/static/media/map.47494475.svg"
+  },
+  {
+    "revision": "546bbc6967f24f5661f9e2bdcd19ad8b",
+    "url": "/the-known-world/index.html"
+  }
+];
